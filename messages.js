80c8700a5a87568605ee651da71bf8326f071acf
rpdud8883 <163230891+rpdud8883@users.noreply.github.com>
2024-03-13 05:59:31 +0000

    2주차 끝!

--- messages.js
+++ messages.js
@@ -2,10 +2,20 @@
 "use strict";
 
 // @TODO: 3개의 문자열 messages 배열 만들어 주세요.
-let messages;
+let messages = [
+    "I'm sleepy",
+    "I'm hungry",
+    "Hello Sleepy, hello hungry, I'm dad."
+];
 
 // @TODO: messages 배열에서 각 메시지를 인쇄하는 함수를 만들어 주세요.
-let printMsgs;
+function printMsgs() {
+    for (let i = 0; i < messages.length; i++) {
+        console.log(messages[i]);
+    }
+}
+
+printMsgs();
 
 
 
